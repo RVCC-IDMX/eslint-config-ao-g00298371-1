2a2e1253e10f612d81b1f17ab4b31f06cb190c4b
ao-g00298371 <g00298371@stu.raritanval.edu>
2023-09-24 15:19:33 -0400

    Fixed with ESLint

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/*
-
-  whoa!    Hold your horses! This is badly formatted code.
-
-*/
+// Whoa! Hold your horses! This is badly formatted code.
 
 const message = 'Hello World';
 
-console.log(message); // this should not show as an error
+console.log(message);
+
+// this should not show as an error
